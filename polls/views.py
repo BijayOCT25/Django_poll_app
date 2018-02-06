bdd53059eb9ab8a72237da90d2c77d9fe5a71e0c
BijayOCT25 <bijay_prince14@yahoo.com>
2018-02-06 16:05:17 +0545

    After 6th part

--- polls/views.py
+++ polls/views.py
@@ -11,7 +11,7 @@ class IndexView(generic.ListView):
 	contest_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-		return Question.objects.filter(pub_date_lte=timezone.now()).order_by('-pub_date')[:5]
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model =  Question
